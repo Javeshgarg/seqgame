0d177b85feafd54ba5d6e7e34e130c680afe84a7
Prashanth <prashanth.evo@gmail.com>
2020-03-24 11:11:54 +0530

    Separated algos

---
+++ client/utils/algos/botmove.js
@@ -0,0 +1,6 @@
+/**
+ * Returns a move by bot. (a card in hand)
+ * board[][] has two possible values null or {id: playerId} representing a coin.
+ * hand[] is array of cards in current hand.
+ */
+export default function makeMove(board, hand) {}
